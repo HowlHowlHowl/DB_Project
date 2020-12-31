2ab68c81d4fee64ebbed99f9d8df225d47d182fa
erika.lena@outlook.it <erika.lena@outlook.it>
2020-12-31 17:19:16 +0100

    form values

--- index.js
+++ index.js
@@ -52,6 +52,12 @@ function getHeader(table) {
 
 function changeTypeMP() {
     let type_mp = $('#tipologia').val();
+    if(type_mp == "-") {
+        $('#div_mangimi').remove();
+        $('#div_fertilizzanti').remove();
+        $('#div_pesticidi').remove();
+        mangimi = []; pesticidi = []; fertilizzanti = [];
+    }
     if(type_mp == 'animale') {
         pesticidi = []; fertilizzanti = [];
         $('#div_fertilizzanti').remove();
@@ -126,184 +132,189 @@ function changeForm() {
     fertilizzanti = []; pesticidi = []; mangimi = [];
     materie_prime = []; lavorazioni = [];
 
-    $('#form_mangimi').remove();
-    if(table =='produttore' || table =='fornitore' || table=='azienda_trasporti') {
-        $('#form').html(`<div class="form-group">
-            <label for="partita_iva">P.I.</label>
-            <input type="text" class="form-control" id="partita_iva" placeholder="11111111111">
-            </div>
-            <div class="form-group">
-                <label for="nome">Nome</label>
-                <input type="text" class="form-control" id="nome" placeholder="">
-            </div>
-            <div class="form-group">
-                <label for="ragione_sociale">Ragione sociale</label>
-                <input type="text" class="form-control" id="ragione_sociale" placeholder="Bianchi & Rossi S.a.s.">
+    if(table == '-') {
+        $('#form').html("");
+    }
+    else {
+        if(table =='produttore' || table =='fornitore' || table=='azienda_trasporti') {
+            $('#form').html(`<div class="form-group">
+                <label for="partita_iva">P.I.</label>
+                <input type="text" class="form-control" id="partita_iva" placeholder="11111111111">
+                </div>
+                <div class="form-group">
+                    <label for="nome">Nome</label>
+                    <input type="text" class="form-control" id="nome" placeholder="">
+                </div>
+                <div class="form-group">
+                    <label for="ragione_sociale">Ragione sociale</label>
+                    <input type="text" class="form-control" id="ragione_sociale" placeholder="Bianchi & Rossi S.a.s.">
+                </div>`);
+        }
+        else if(table == 'prodotto') {
+            $('#form').html(`<div class="form-group">
+                <label for="EAN">Codice EAN</label>
+                <input type="text" class="form-control" id="ean" placeholder="1111111111111">
+                </div>
+                <div class="form-group">
+                    <label for="nome">Nome</label>
+                    <input type="text" class="form-control" id="nome" placeholder="Mozzarella">
+                </div>
+                <div class="form-group">
+                    <label for="peso">Peso</label>
+                    <input type="text" class="form-control" id="peso" placeholder="200g">
+                </div>
+                <div class="form-group">
+                    <label for="data">Data</label>
+                    <input type="text" class="form-control" id="data" placeholder="30/12/2020">
+                </div>
+                <div class="form-group">
+                    <label for="azienda_trasporti">Azienda di trasporto</label>
+                    <input type="text" class="form-control" id="azienda_trasporti" placeholder="">
+                </div>
+                <div class="form-group">
+                    <label for="tipo_trasporto">Tipo di trasporto</label>
+                    <input type="text" class="form-control" id="tipo_trasporto" placeholder="">
+                </div>
+                <div class="form-group">
+                    <label for="package">Package</label>
+                    <input type="text" class="form-control" id="package" placeholder="">
+                </div>
+                <div class="form-group">
+                    <label for="produttore">Azienda produttrice</label>
+                    <input type="text" class="form-control" id="azienda_produttrice" placeholder="">
+                </div>
+                `);
+    
+            $('#parameters').append(`
+            <div id="div_materie_prime">
+            <form id="form_materie_prime">
+                <p> Aggiungi le materie prime che compongono il prodotto </p>
+                <div class="form-group">
+                    <label for="nome_materia_prima">Nome</label>
+                    <input type="text" class="form-control" id="nome_materia_prima" placeholder="">
+                </div>
+                <div class="form-group">
+                    <label for="luogo_materia_prima">Luogo</label>
+                    <input type="text" class="form-control" id="luogo_materia_prima" placeholder="">
+                </div>
+                <input type="submit" value="Aggiungi">
+            </form>
+            <ul id="lista_materie_prime"></ul>
             </div>`);
-    }
-    else if(table == 'prodotto') {
-        $('#form').html(`<div class="form-group">
-            <label for="EAN">Codice EAN</label>
-            <input type="text" class="form-control" id="ean" placeholder="1111111111111">
-            </div>
-            <div class="form-group">
-                <label for="nome">Nome</label>
-                <input type="text" class="form-control" id="nome" placeholder="Mozzarella">
-            </div>
-            <div class="form-group">
-                <label for="peso">Peso</label>
-                <input type="text" class="form-control" id="peso" placeholder="200g">
-            </div>
-            <div class="form-group">
-                <label for="data">Data</label>
-                <input type="text" class="form-control" id="data" placeholder="30/12/2020">
-            </div>
-            <div class="form-group">
-                <label for="azienda_trasporti">Azienda di trasporto</label>
-                <input type="text" class="form-control" id="azienda_trasporti" placeholder="">
-            </div>
-            <div class="form-group">
-                <label for="tipo_trasporto">Tipo di trasporto</label>
-                <input type="text" class="form-control" id="tipo_trasporto" placeholder="">
-            </div>
-            <div class="form-group">
-                <label for="package">Package</label>
-                <input type="text" class="form-control" id="package" placeholder="">
-            </div>
-            <div class="form-group">
-                <label for="produttore">Azienda produttrice</label>
-                <input type="text" class="form-control" id="azienda_produttrice" placeholder="">
-            </div>
-            `);
-
-        $('#parameters').append(`
-        <div id="div_materie_prime">
-        <form id="form_materie_prime">
-            <p> Aggiungi le materie prime che compongono il prodotto </p>
-            <div class="form-group">
-                <label for="nome_materia_prima">Nome</label>
-                <input type="text" class="form-control" id="nome_materia_prima" placeholder="">
-            </div>
-            <div class="form-group">
-                <label for="luogo_materia_prima">Luogo</label>
-                <input type="text" class="form-control" id="luogo_materia_prima" placeholder="">
-            </div>
-            <input type="submit" value="Aggiungi">
-        </form>
-        <ul id="lista_materie_prime"></ul>
-        </div>`);
-        
-
-        $('#parameters').append(`
-        <div id="div_lavorazione">
-        <form id="form_lavorazioni">
-            <p> Aggiungi le lavorazioni del prodotto </p>
-            <div class="form-group">
-                <label for="nome_lavorazioni">Nome</label>
-                <input type="text" class="form-control" id="nome_lavorazioni" placeholder="">
-            </div>
-            <input type="submit" value="Aggiungi">
-        </form>
-        <ul id="lista_lavorazioni"></ul>
-        </div>`);
-        
-    }
-    else if(table == 'materia_prima') {
-        $('#form').html(`<div class="form-group">
-            <div class="form-group">
-                <label for="nome">Nome</label>
-                <input type="text" class="form-control" id="nome" placeholder="">
-            </div>
-            <div class="form-group">
-                <label for="luogo">Luogo</label>
-                <input type="text" class="form-control" id="peso" placeholder="">
-            </div>
-            <div class="form-group">
-                <label for="tipologia">Tipologia</label>
-                <select id="tipologia">
-                    <option value="-">-</option>
-                    <option value="animale">Animale</option>
-                    <option value="vegetale">Vegetale</option>
-                </select>
-            </div>
-            <div class="form-group">
-                <label for="q_terra">Superficie di terreno utilizzata</label>
-                <input type="text" class="form-control" id="q_terra" placeholder="">
-            </div>
-            <div class="form-group">
-                <label for="q_acqua">Quantità di acqua</label>
-                <input type="text" class="form-control" id="q_acqua" placeholder="">
-            </div>
-            <div class="form-group">
-                <label for="CO2">Quantità di gas serra prodotta (CO2 eq.)</label>
-                <input type="text" class="form-control" id="CO2 placeholder="">
-            </div>
-            <div class="form-group">
-                <label for="fornitore">Fornitore</label>
-                <input type="text" class="form-control" id="fornitore" placeholder="">
+            
+    
+            $('#parameters').append(`
+            <div id="div_lavorazione">
+            <form id="form_lavorazioni">
+                <p> Aggiungi le lavorazioni del prodotto </p>
+                <div class="form-group">
+                    <label for="nome_lavorazioni">Nome</label>
+                    <input type="text" class="form-control" id="nome_lavorazioni" placeholder="">
+                </div>
+                <input type="submit" value="Aggiungi">
+            </form>
+            <ul id="lista_lavorazioni"></ul>
             </div>`);
-    }
-    else if(table == 'package') {
-        $('#form').html(`<div class="form-group">
-            <label for="codice">Codice package</label>
-            <input type="text" class="form-control" id="codice" placeholder="">
-            </div>
-            <div class="form-group">
-                <label for="tipo">Tipo</label>
+            
+        }
+        else if(table == 'materia_prima') {
+            $('#form').html(`<div class="form-group">
+                <div class="form-group">
+                    <label for="nome">Nome</label>
+                    <input type="text" class="form-control" id="nome" placeholder="">
+                </div>
+                <div class="form-group">
+                    <label for="luogo">Luogo</label>
+                    <input type="text" class="form-control" id="peso" placeholder="">
+                </div>
+                <div class="form-group">
+                    <label for="tipologia">Tipologia</label>
+                    <select id="tipologia">
+                        <option value="-">-</option>
+                        <option value="animale">Animale</option>
+                        <option value="vegetale">Vegetale</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label for="q_terra">Superficie di terreno utilizzata</label>
+                    <input type="text" class="form-control" id="q_terra" placeholder="">
+                </div>
+                <div class="form-group">
+                    <label for="q_acqua">Quantità di acqua</label>
+                    <input type="text" class="form-control" id="q_acqua" placeholder="">
+                </div>
+                <div class="form-group">
+                    <label for="CO2">Quantità di gas serra prodotta (CO2 eq.)</label>
+                    <input type="text" class="form-control" id="CO2 placeholder="">
+                </div>
+                <div class="form-group">
+                    <label for="fornitore">Fornitore</label>
+                    <input type="text" class="form-control" id="fornitore" placeholder="">
+                </div>`);
+        }
+        else if(table == 'package') {
+            $('#form').html(`<div class="form-group">
+                <label for="codice">Codice package</label>
+                <input type="text" class="form-control" id="codice" placeholder="">
+                </div>
+                <div class="form-group">
+                    <label for="tipo">Tipo</label>
+                    <input type="text" class="form-control" id="tipo" placeholder="">
+                </div>
+                <div class="form-group">
+                    <label for="materiale">Materiale utilizzato</label>
+                    <input type="text" class="form-control" id="materiale" placeholder="">
+                </div>
+                <div class="form-group">
+                    <label for="volume">Volume occupato</label>
+                    <input type="text" class="form-control" id="volume" placeholder="">
+                </div>
+                <div class="form-group">
+                    <label for="peso">Peso del package</label>
+                    <input type="text" class="form-control" id="azienda_trasporti" placeholder="">
+                </div>`);
+        }
+        else if(table == 'procedura_lavorazione') {
+            $('#form').html(`<div class="form-group">
+                <label for="tipo">Tipologia di lavorazione</label>
                 <input type="text" class="form-control" id="tipo" placeholder="">
-            </div>
-            <div class="form-group">
-                <label for="materiale">Materiale utilizzato</label>
-                <input type="text" class="form-control" id="materiale" placeholder="">
-            </div>
-            <div class="form-group">
-                <label for="volume">Volume occupato</label>
-                <input type="text" class="form-control" id="volume" placeholder="">
-            </div>
-            <div class="form-group">
-                <label for="peso">Peso del package</label>
-                <input type="text" class="form-control" id="azienda_trasporti" placeholder="">
-            </div>`);
+                </div>
+                <div class="form-group">
+                    <label for="CO2">Quantità di gas serra prodotta (CO2 eq.)</label>
+                    <input type="text" class="form-control" id="CO2" placeholder="">
+                </div>
+                <div class="form-group">
+                    <label for="q_acqua">Quantità di acqua</label>
+                    <input type="text" class="form-control" id="q_acqua" placeholder="">
+                </div>`);
+        }
+        else if(table == 'fertilizzante' || table == 'pesticida') {
+            $('#form').html(`<div class="form-group">
+                <label for="nome">Nome</label>
+                <input type="text" class="form-control" id="nome" placeholder="">
+                </div>
+                <div class="form-group">
+                    <label for="acidificazione">Valore di acidificazione</label>
+                    <input type="text" class="form-control" id="CO2" placeholder="">
+                </div>
+                <div class="form-group">
+                    <label for="eutrofizzazione">Valore di eutrofizzazione</label>
+                    <input type="text" class="form-control" id="eutrofizzazione" placeholder="">
+                </div>`);
+        }
+        else if(table == 'mangime') {
+            $('#form').html(`<div class="form-group">
+                <label for="nome">Nome</label>
+                <input type="text" class="form-control" id="nome" placeholder="">
+                </div>
+                <div class="form-group">
+                    <label for="componente">Componente principale</label>
+                    <input type="text" class="form-control" id="componente" placeholder="farine di cereali">
+                </div>`);
+        }
+        $('#form').prepend('<p>Campi di inserimento</p><input type="submit" value="Inserisci elemento">');
     }
-    else if(table == 'procedura_lavorazione') {
-        $('#form').html(`<div class="form-group">
-            <label for="tipo">Tipologia di lavorazione</label>
-            <input type="text" class="form-control" id="tipo" placeholder="">
-            </div>
-            <div class="form-group">
-                <label for="CO2">Quantità di gas serra prodotta (CO2 eq.)</label>
-                <input type="text" class="form-control" id="CO2" placeholder="">
-            </div>
-            <div class="form-group">
-                <label for="q_acqua">Quantità di acqua</label>
-                <input type="text" class="form-control" id="q_acqua" placeholder="">
-            </div>`);
-    }
-    else if(table == 'fertilizzante' || table == 'pesticida') {
-        $('#form').html(`<div class="form-group">
-            <label for="nome">Nome</label>
-            <input type="text" class="form-control" id="nome" placeholder="">
-            </div>
-            <div class="form-group">
-                <label for="acidificazione">Valore di acidificazione</label>
-                <input type="text" class="form-control" id="CO2" placeholder="">
-            </div>
-            <div class="form-group">
-                <label for="eutrofizzazione">Valore di eutrofizzazione</label>
-                <input type="text" class="form-control" id="eutrofizzazione" placeholder="">
-            </div>`);
-    }
-    else if(table == 'mangime') {
-        $('#form').html(`<div class="form-group">
-            <label for="nome">Nome</label>
-            <input type="text" class="form-control" id="nome" placeholder="">
-            </div>
-            <div class="form-group">
-                <label for="componente">Componente principale</label>
-                <input type="text" class="form-control" id="componente" placeholder="farine di cereali">
-            </div>`);
-    }
-    $('#form').prepend('<input type="submit" value="Inserisci elemento">');
+   
 }
 function insertElement() {
     let table = $('#tables').val();
@@ -350,20 +361,20 @@ $(document).on('submit','#form_mangimi',  (event) =>{
 
 $(document).on('submit','#form_fertilizzanti', (event) =>{
     event.preventDefault();
-    fertilizzanti.push({fertilizzante: $('#fertilizzante').val(), quantita: $('#quantita_fert').val()});
+    fertilizzanti.push({nome: $('#fertilizzante').val(), quantita: $('#quantita_fert').val()});
     $("#lista_fertilizzanti").html("");
     fertilizzanti.forEach( (item) => {
-        $("#lista_fertilizzanti").append(`<li> ${item.fertilizzante} ${item.quantita}</li>`)
+        $("#lista_fertilizzanti").append(`<li> ${item.nome} ${item.quantita}</li>`)
     })
     $('#form_fertilizzanti')[0].reset();
 });
 
 $(document).on('submit','#form_pesticidi', (event) =>{
     event.preventDefault();
-    pesticidi.push({pesticida: $('#pesticida').val(), quantita: $('#quantita_pesticida').val()});
+    pesticidi.push({nome: $('#pesticida').val(), quantita: $('#quantita_pesticida').val()});
     $("#lista_pesticidi").html("");
     pesticidi.forEach( (item) => {
-        $("#lista_pesticidi").append(`<li> ${item.pesticida} ${item.quantita}</li>`)
+        $("#lista_pesticidi").append(`<li> ${item.nome} ${item.quantita}</li>`)
     })
     $('#form_pesticidi')[0].reset();
 });
